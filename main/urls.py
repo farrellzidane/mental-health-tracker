c27c751127234aecb6179498a8293c5af29e2aa2
farrellzidane <raihandrawan@gmail.com>
2024-11-13 14:20:47 +0700

    Finished Tutorial 8 Flutter

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_mood_entry, add_mood_entry_ajax, edit_mood, delete_mood, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import show_main, create_mood_entry, add_mood_entry_ajax, edit_mood, delete_mood, create_mood_flutter, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 app_name = 'main'
 
 urlpatterns = [
@@ -14,5 +14,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'),
-    path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax')
+    path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter')
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 import datetime
+import json
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.urls import reverse
 from django.shortcuts import render, redirect, reverse  # Tambahkan import redirect di baris ini
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -114,6 +115,24 @@ def delete_mood(request, id):
     mood.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
 
 def show_xml(request):
     data = MoodEntry.objects.filter(user=request.user)
